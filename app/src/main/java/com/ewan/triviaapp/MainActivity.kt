6ffc10e632fd1bb4ae6bd7375a4ea2eb2ecd6e15
ewanlew <97612083+ewanlew@users.noreply.github.com>
2024-11-27 19:19:33 +0000

    not crashing anymore!

--- app/src/main/java/com/ewan/triviaapp/MainActivity.kt
+++ app/src/main/java/com/ewan/triviaapp/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.ewan.triviaapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("MainActivity", "onCreate started!")
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        Log.d("MainActivity", "setContentView done!")
+
+        val recyclerView = findViewById<RecyclerView>(R.id.userList)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
