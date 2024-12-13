afd399efff6ee41e1bc4c8529a23ee4198784d1e
ewanlew <97612083+ewanlew@users.noreply.github.com>
2024-12-13 00:14:18 +0000

    Preferences Screen

---
+++ app/src/main/java/com/ewan/triviaapp/activities/PreferencesActivity.kt
@@ -0,0 +1,13 @@
+package com.ewan.triviaapp.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ewan.triviaapp.R
+
+class PreferencesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preferences)
+    }
+}
